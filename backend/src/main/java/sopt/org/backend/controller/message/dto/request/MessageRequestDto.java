563ed599a425bd532e64450c755dee23cae3c34f
유서린 <sprout3082@gmail.com>
2023-05-21 04:45:58 +0900

    [refer] : message 요청 content 사이즈 예외처리

--- backend/src/main/java/sopt/org/backend/controller/message/dto/request/MessageRequestDto.java
+++ backend/src/main/java/sopt/org/backend/controller/message/dto/request/MessageRequestDto.java
@@ -3,12 +3,15 @@ package sopt.org.backend.controller.message.dto.request;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import javax.validation.constraints.Max;
+
 @Getter
 @AllArgsConstructor
 public class MessageRequestDto {
 
     private final String roomName;
 
+    @Max(100)
     private String content;
 
 }
