d246e507124bb6d758a9bdddae6d5c371ce7ab3b
유서린 <sprout3082@gmail.com>
2023-05-21 00:56:28 +0900

    [fix] : Room Service 구현

--- backend/src/main/java/sopt/org/backend/controller/room/RoomController.java
+++ backend/src/main/java/sopt/org/backend/controller/room/RoomController.java
@@ -1,4 +1,13 @@
 package sopt.org.backend.controller.room;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+import sopt.org.backend.service.RoomService;
+
+@RestController
+@RequiredArgsConstructor
 public class RoomController {
+
+    private final RoomService roomService;
+
 }
--- backend/src/main/java/sopt/org/backend/infrastructure/message/MessageRepository.java
+++ backend/src/main/java/sopt/org/backend/infrastructure/message/MessageRepository.java
@@ -3,5 +3,5 @@ package sopt.org.backend.infrastructure.message;
 import org.springframework.data.jpa.repository.JpaRepository;
 import sopt.org.backend.domain.message.Message;
 
-public interface MessageRepository extends JpaRepository<Long, Message> {
+public interface MessageRepository extends JpaRepository<Message, Long> {
 }
